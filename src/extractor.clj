9a94f5f5250e6a7b47a9c9e41ff46755bc1d7f96
Rafael Delboni <rafadelboni@gmail.com>
2023-03-14 23:20:06 -0300

    feat: implements json encoded files

--- src/extractor.clj
+++ src/extractor.clj
@@ -2,7 +2,8 @@
   (:require [clj-kondo.core :as kondo]
             [clojure.edn :as edn]
             [clojure.java.io :as io :refer [make-parents]]
-            [clojure.tools.deps :as deps]))
+            [clojure.tools.deps :as deps]
+            [cheshire.core :as cheshire]))
 
 (defn get-jar
   [project version]
@@ -34,12 +35,13 @@
       (update :ns #(some-> % str))))
 
 (defn var-defs->file-name
-  [project namespace]
-  (str project "/" namespace ".edn"))
+  [project namespace output]
+  (str project "/" namespace "." (name output)))
 
 (defn extract-analysis!
-  [project version]
-  (let [{:keys [var-definitions namespace-definitions]} (kondo-run! [(get-jar project version)])]
+  [project version output]
+  (let [{:keys [var-definitions namespace-definitions]}
+        (kondo-run! [(get-jar project version)])]
     {:vars (->> var-definitions
                 (map kondo-analysis->analysis)
                 (group-by :ns))
@@ -48,36 +50,45 @@
                       kondo-analysis->analysis
                       (as-> adapted-ns
                             (assoc adapted-ns
-                                   :var-definitions (var-defs->file-name project (:name adapted-ns))))))
+                                   :var-definitions (var-defs->file-name project (:name adapted-ns) output)))))
                 namespace-definitions)}))
 
 (defn analysis->file!
-  [analysis file-name]
+  [analysis file-name output]
   (make-parents file-name)
-  (spit file-name analysis))
+  (->> (case output
+         :json (cheshire/generate-string analysis)
+         analysis)
+       (spit file-name)))
 
 (defn extract-all!
-  [projects]
+  [projects output]
   (doseq [{:keys [project version]} projects]
     (println "starting extract " project ":" version)
-    (let [{:keys [vars nss]} (extract-analysis! project version)]
-      (analysis->file! nss (str project ".edn"))
+    (let [{:keys [vars nss]} (extract-analysis! project version output)]
+      (analysis->file! nss (str project "." (name output)) output)
       (doseq [[k v] vars]
-        (analysis->file! v (var-defs->file-name project k))))
+        (analysis->file! v (var-defs->file-name project k output) output)))
     (println "finished " project ":" version)))
 
 (defn read-edn [file-name]
   (-> (slurp file-name)
       (edn/read-string)))
 
+(defn read-json [file-name]
+  (-> (slurp file-name)
+      (cheshire/parse-string true)))
+
 (comment
   ;; downloads and process the projects listed
   (extract-all! [{:project 'org.clojure/clojure
                   :version "1.11.1"}
                  {:project 'org.clojure/clojurescript
-                  :version "1.11.60"}])
+                  :version "1.11.60"}]
+                :json) ; or :edn
 
   ;; reading the produced files
+  (read-json "org.clojure/clojure.json")
   (read-edn "org.clojure/clojure.edn")
   (let [namespace (->> (read-edn "org.clojure/clojurescript.edn")
                        (filter #(= (:name %) "cljs.core"))
